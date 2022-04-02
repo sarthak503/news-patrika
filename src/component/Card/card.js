d84ed55d0093c2835013eb476866f60c769ffc22
sarthak503 <sarthak.kumar503@gmail.com>
2022-04-02 23:19:07 +0530

    Added alternative tag

--- src/component/Card/card.js
+++ src/component/Card/card.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import CardActions from '@mui/material/CardActions';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export default function ActionAreaCard() {
   return (
@@ -15,7 +15,7 @@ export default function ActionAreaCard() {
           component="img"
           height="140"
           image={require("./img_title.jpg")}
-        //   alt="green iguana"
+          alt="green iguana"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
@@ -27,8 +27,8 @@ export default function ActionAreaCard() {
           </Typography>
         </CardContent>
         <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
       </CardActionArea>
     </Card>
   );
