a8c677d6da0669397982e5b077ca2a3327592bfb
sarthak503 <sarthak.kumar503@gmail.com>
2022-04-03 11:00:24 +0530

    added css and made responsive

--- src/component/Card/card.js
+++ src/component/Card/card.js
@@ -6,32 +6,36 @@ import Button from "@mui/material/Button";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import "./Card.css";
 
-const NewsCard = (Item) => {
+const NewsCard = ({ Item }) => {
+  console.log("xsasa", Item);
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={Item.urlToimage}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {Item.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {Item.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button href="{Item.url}" size="small">
-            Learn More
-          </Button>
-        </CardActions>
-      </CardActionArea>
-    </Card>
+    <div className="cardStyle">
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={Item.urlToImage}
+            //alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {Item.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {Item.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button href={Item.url} size="small">
+              Learn More
+            </Button>
+          </CardActions>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 };
 export default NewsCard;
